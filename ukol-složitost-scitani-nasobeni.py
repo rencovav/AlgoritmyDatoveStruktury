95b507e18988b5d2e9d8b295054c0c9592388b3c
Vlasta Rencova <rencovav@seznam.cz>
2022-03-13 11:28:19 +0100

    složitost algoritmu sčítání a násobení dvou čísel

---
+++ ukol-složitost-scitani-nasobeni.py
@@ -0,0 +1,11 @@
+# sčítání dvou čísel
+a = 5
+b = 3
+c = a + b
+
+# násobení dvou čísel
+d = a * b
+
+"""
+Sčítání a násobení dvou čísel má složitost O(1), protože se jedná vždy pouze o 1 operaci mezi dvěma čísly. 
+"""
